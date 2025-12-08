e79ba68fd19cfa67a9b189cffd105511a107ae70
TatuNCP <76669149+TatuNCP@users.noreply.github.com>
2025-12-08 18:49:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -51,6 +51,8 @@ loadCart();
 
 // 1. DATA LOADING AND DOM READY
 document.addEventListener('DOMContentLoaded', () => {
+
+    emailjs.init("frfWoBbmJwoPNo-qm");
     // 1.1 Carga Inicial de datos
     cargarCatalogo();
 
